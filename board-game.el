e2cc14001da85a6a283e8a1c69e2fee9537c8208
taf <taf@ce.com>
2018-04-07 03:40:33 +0200

    added URL, so that people can access code.

--- board-game.el
+++ board-game.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2018
 
 ;; Author: Florian Tauber
+;; URL: https://github.com/computeremotion/board-game
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
